962316de12fa1845b33722825c7d30991e7f1736
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2023-05-26 12:58:57 -0400

    Added Modifier

--- src/javacards/Card.java
+++ src/javacards/Card.java
@@ -12,8 +12,14 @@ package javacards;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye & Nayan
+ * @author dancye
  */
+
+/*
+ * Modifier: Nayan Mapara
+ * Student Number: 991701339
+ */
+
 public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
