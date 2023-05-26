dd8d4750831164d66ccad91a32db3537ef1f42d7
hp <hp@10.48.58.190>
2023-05-26 12:00:48 -0400

    rrr

--- src/javacards/CardTrick.java
+++ src/javacards/CardTrick.java
@@ -10,23 +10,36 @@ package javacards;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modified by Gagandeep Singh #991709762
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+
+    public static void main(String[] args) {
+        Card[] magicDeck = new Card[7];
+
+        for (int i = 0; i < magicDeck.length; i++) {
+            Card card = new Card();
+            card.setValue((int) (Math.random() * 13) + 1);
+            card.setSuit(Card.SUITS[(int) (Math.random() * 4)]);
+            magicDeck[i] = card;
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+
+        Card userCard = new Card();
+        userCard.setValue(7);
+        userCard.setSuit(Card.SUITS[2]);
+        boolean isCardFound = false;
+
+        for (int i = 0; i < magicDeck.length; i++) {
+            if (userCard.getSuit().equals(magicDeck[i].getSuit()) && magicDeck[i].getValue() == userCard.getValue()) {
+                isCardFound = true;
+                break;
+            }
+        }
+
+        if (isCardFound) {
+            System.out.println("Congratulations! Your card was found in the magic deck!");
+        } else {
+            System.out.println("Sorry! Your card was not found in the magic deck.");
+        }
     }
-    
 }
