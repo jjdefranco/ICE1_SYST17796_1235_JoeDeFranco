0ef8158025e350e1b84ac06a75281df54cd44e3c
akshitas1701 <134462924+akshitas1701@users.noreply.github.com>
2023-05-25 22:26:31 -0400

    Update CardTrick.java

--- src/javacards/CardTrick.java
+++ src/javacards/CardTrick.java
@@ -24,7 +24,14 @@ public class CardTrick {
             c.setSuit(Card.SUITS[generateRandomNumber(0, 3)]);
             magic[i] = c;
         }
+        
+        Card luckyCard = new Card();
+        luckyCard.setValue(10); // Example: Set the lucky card value to 10
+        luckyCard.setSuit("Hearts"); // Example: Set the lucky card suit to Hearts
 
+        boolean foundUserCard = false;
+        boolean foundLuckyCard = false;
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Pick a card value: ");
         int value = scanner.nextInt();
@@ -45,12 +52,28 @@ public class CardTrick {
             }
         }
 
-        if (found) {
-            System.out.println("Congratulations! Your card is in the magic hand.");
-        } else {
-            System.out.println("Sorry, your card is not in the magic hand.");
+       if (foundUserCard && foundLuckyCard) {
+           
+            System.out.println("Congratulations! You found your card and the lucky card!");
+           
+        }
+        else if (foundUserCard) {
+            
+            System.out.println("Congratulations! You found your card, but not the lucky card.");
+            
+        } 
+        else if (foundLuckyCard) {
+            
+            System.out.println("Sorry, you didn't find your card, but you found the lucky card!");
+            
+        } 
+        else {
+            
+            System.out.println("Sorry, you neither find your card nor the lucky card.");
+            
         }
     }
+
 
     private static int generateRandomNumber(int min, int max) {
         return min + (int) (Math.random() * (max - min + 1));
