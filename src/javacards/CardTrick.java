81aff8d9b19ec7b5db9be7a1d3d47fde21a1efff
Adison0301 <macwanad@sheridancollege.ca>
2023-05-26 09:09:51 -0400

    Full code completed

--- src/javacards/CardTrick.java
+++ src/javacards/CardTrick.java
@@ -8,10 +8,7 @@ import java.lang.*;
 import java.util.*;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * @author Adison Dickans Macwan
  */
 public class CardTrick {
     
@@ -31,8 +28,7 @@ public class CardTrick {
             magicHand[i] = new Card();
             magicHand[i].setValue(randomNumber);
             magicHand[i].setSuit(Card.SUITS[randomSuit]);
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+         
         }
         System.out.println("Please input a card number:");
         userCard = input.nextInt();
@@ -58,9 +54,7 @@ public class CardTrick {
             System.out.println("Your card is not in the magic hand");
             
         }
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        
     }
     
 }
