d7f055edb8b84a93462fb49baa0cda70b1d62e7b
akshitasharma <akshitasharma@localhost>
2023-05-25 22:08:40 -0400

    Edited CardTrick class

--- src/javacards/CardTrick.java
+++ src/javacards/CardTrick.java
@@ -5,28 +5,58 @@
  */
 package javacards;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * for the match to the user's card.
+ * Created by Akshita Sharma (Modifier: 991706738).
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+
+    public static void main(String[] args) {
+        Card[] magic = new Card[7];
+
+        for (int i = 0; i < magic.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(generateRandomValue());
+            c.setSuit(Card.SUITS[generateRandomNumber(0, 3)]);
+            magic[i] = c;
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pick a card value: ");
+        int value = scanner.nextInt();
+        scanner.nextLine(); 
+
+        System.out.print("Pick a card suit (Hearts, Diamonds, Clubs, Spades): ");
+        String suit = scanner.nextLine();
+
+        Card choosenCard = new Card();
+        choosenCard.setValue(value);
+        choosenCard.setSuit(suit);
+
+        boolean found = false;
+        for (Card card : magic) {
+            if (card.getValue() == choosenCard.getValue() && card.getSuit().equalsIgnoreCase(choosenCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
     }
-    
+
+    private static int generateRandomNumber(int min, int max) {
+        return min + (int) (Math.random() * (max - min + 1));
+    }
+// Generating random values between 2 and 14 (inclusive)
+    private static int generateRandomValue() {
+        return generateRandomNumber(2, 14); 
+    }
 }
