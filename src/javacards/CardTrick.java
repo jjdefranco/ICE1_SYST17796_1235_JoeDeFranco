787effe1178687e85cdfb8a909cd091377a6e7d2
Muskan <muskan50@sheridancollege.ca>
2023-05-26 22:52:38 -0400

    merged master for the donkey

--- src/javacards/CardTrick.java
+++ src/javacards/CardTrick.java
@@ -11,22 +11,45 @@ package javacards;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  */
+import java.util.*;
+
+
 public class CardTrick {
     
-    public static void main(String[] args)
+    public static void main(String[] args)    
     {
         Card[] magicHand = new Card[7];
-        
+        boolean cardFound = false;
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            magicHand[i] = new Card();
+            magicHand[i].setValue(generateRandomNum(0,9));
+            magicHand[i].setSuit(Card.SUITS[generateRandomNum(0,3)]);
         }
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter value for the card");
+        int cardValue=scan.nextInt();
+        System.out.println("Enter value for the suit");
+        String suitValue = scan.next();
         
+
         //insert code to ask the user for Card value and suit, create their card
+        for(Card i: magicHand){
+            if((i.getSuit().equals(suitValue)) && (i.getValue() == cardValue)){
+                cardFound = true;
+            }
+        }
+        if(cardFound){
+            System.out.println("CONGO!yOUR CARD IS IN THE MAGIC HAND");
+        }else{
+            System.out.println("ALAS!yOUR CARD IS NOT IN THE MAGIC HAND");
+        }
         // and search magicHand here
         //Then report the result here
+    }
+    
+    public static int generateRandomNum(int start, int end){
+        return start + (int)(Math.random() * ((end - start) + 1));
     }
     
 }
