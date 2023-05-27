c0dd7dd4ea44d368f4d5bac1f891902745d1c6f5
Master Hasan <123147147+MasterHasan095@users.noreply.github.com>
2023-05-26 21:25:06 -0400

    hardcoded a luckyCard object
    I commented the area where we took input from user and added an object luckyCard as asked by sir keeping the value as Ace of Hearts

--- src/javacards/CardTrick.java
+++ src/javacards/CardTrick.java
@@ -30,8 +30,13 @@ public class CardTrick {
         Card[] magicHand = new Card[7]; // An array of objects
         Random random = new Random(); // To generate random cards
         Scanner input = new Scanner(System.in); //To take value from users
+        Card luckyCard = new Card(); // Our hard Coded Card
+        
+        luckyCard.setSuit("Hearts"); // To hard code the suit of our special card object
+        luckyCard.setValue(1);// To hard code the value of our special card object
 
-
+        /* Second step of the exercise where we hard code our card, this is the reason i commented all this.
+        
         System.out.println("You can choose any card from the decks, options for the user guidance are as follows :");
         System.out.println("A card can have one of the following list of suits: clubs, hearts, spades, diamonds");
         System.out.println("A card can have a value from 1-13 with the specifications as Ace = 1, Jack -11, Queen =12, King = 13");
@@ -39,7 +44,7 @@ public class CardTrick {
         String userSuit = input.next(); //Takes the suit value from user
         System.out.print("Enter the card value : ");
         int userValue = input.nextInt(); // Takes the card value from the user
-
+        */
 
 
         for (int i = 0; i < magicHand.length; i++) {
@@ -50,13 +55,15 @@ public class CardTrick {
             int randomSuit = random.nextInt(4);
             c.setSuit(Card.SUITS[randomSuit]); //Sets the Suit of a card in magicHand Array
         }
+        
+        
 
 
         String matchCheck = "N"; //To avoid using a method with a return value, we use a pre defined variable and change its value when the card matches
         for (int i =0; i< magicHand.length; i++){
-            if (magicHand[i].getSuit().equalsIgnoreCase(userSuit)){
-                if(magicHand[i].getValue() == userValue){
-                    System.out.println("Succesful, card matched");
+            if (magicHand[i].getSuit().equalsIgnoreCase(luckyCard.getSuit())){
+                if(magicHand[i].getValue() == luckyCard.getValue()){
+                    System.out.println("Yay, you win!!!"); // Winning message
                     matchCheck = "Y"; //We change the value of the variable so it doesnt print Did not match
                     break; // Breaks the loop even if the card matches once
                 }
@@ -64,7 +71,7 @@ public class CardTrick {
         }
         
         if (matchCheck == "N"){
-            System.out.println("Did not match"); // Only works when the card by user is not included in the magicHand Array
+            System.out.println("OOPS, You lost"); // Losing Message. // Only works when the card by user is not included in the magicHand Array
         }
     }
     
