0ac4974994cfed50f801856885d8ecb76821c864
chapranj <chapranj@sheridancollege.ca>
2023-05-26 21:53:12 -0400

    added a String for the algorithm to work and added some comments

--- src/javacards/CardTrick.java
+++ src/javacards/CardTrick.java
@@ -17,35 +17,41 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        //Generating Random Card object in the for loop below...
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             magicHand[i]=c;
             //c.setValue(insert call to random number generator here)
-            c.setValue((int)(Math.random()*13));
+            c.setValue((int)(Math.random()*13)+1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             c.setSuit(Card.SUITS[(int)(Math.random()*3)]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
         Scanner input=new Scanner(System.in);
+        System.out.println("You can select the Card value from 1 to 13");
         System.out.println("Enter the card value: ");
         int cardValue=input.nextInt();
+        System.out.println("You can select the Suit from {Hearts, Clubs, Spades, Diamonds}");
         System.out.println("Enter the Suit: ");
         String suit=input.next();
         Card c2=new Card();
         c2.setValue(cardValue);
         c2.setSuit(suit);
-        
+        //Checking if the card matched the random Card object or not..
+        String checkThis="N";
         for(int i=0;i<magicHand.length;i++){
             if((magicHand[i].getValue()==c2.getValue())&&(magicHand[i].getSuit().equalsIgnoreCase(c2.getSuit()))){
                 System.out.println("Card Matched!");
+                checkThis="Y";
                 break;
             }
-            else
-                System.out.println("Card Did not match");
         }
+        if(checkThis=="N"){
+            System.out.println("Card did not match!");
+        }
+        
         // and search magicHand here
         //Then report the result here
     }
