cf48a6fd51f7180d0fb4c6bb2a32b10f7c050682
TalhaAWOL <mtalhaibne@gmail.com>
2023-06-01 17:17:56 -0400

    Added Lucky Card Functionality

--- src/javacards/CardTrick.java
+++ src/javacards/CardTrick.java
@@ -1,4 +1,4 @@
-/*
+/* Name: Talha Mahmood, Date 25/5/2023, Student ID:991723754
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
